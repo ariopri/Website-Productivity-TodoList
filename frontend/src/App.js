9d5b5c9c394bd97b82fca46ac2bca52ed6ccd906
ariopri <arioprima1@gmail.com>
2022-10-17 10:45:22 +0700

    fix : fix blank page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,14 @@ import NavigationBar from "./components/NavigationBar";
 import Fitur from "./components/BerandaItem/Fitur";
 import Testimoni from "./components/BerandaItem/Testimoni";
 import Beranda from "./components/Beranda";
+import Footer from "./components/BerandaItem/Footer";
 
 function App() {
   return (
     <>
       <NavigationBar />
       <Beranda />
+      <Footer />
     </>
   );
 };
