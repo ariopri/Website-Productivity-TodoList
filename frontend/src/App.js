a5872109ce989f4c6735f0ca547c547a2d2c5f7c
ariopri <arioprima1@gmail.com>
2022-10-17 11:20:45 +0700

    docs : setup react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,12 +4,19 @@ import Fitur from "./components/BerandaItem/Fitur";
 import Testimoni from "./components/BerandaItem/Testimoni";
 import Beranda from "./components/Beranda";
 import Footer from "./components/BerandaItem/Footer";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
       <NavigationBar />
-      <Beranda />
+     <Router>
+      <Routes>
+        <Route path="/" element={<Beranda />} />
+        <Route path="/fitur" element={<Fitur />} />
+        <Route path="/testimoni" element={<Testimoni />} />
+      </Routes>
+     </Router>
       <Footer />
     </>
   );
